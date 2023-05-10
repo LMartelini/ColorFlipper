1b68009ec6669962939d9ec6f322a80a7634011f
Laís <laismartelini03@gmail.com>
2023-05-10 16:58:15 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let botao = document.querySelector('button').onclick = clicar;
+let div = document.querySelector('.espaço');
+
+let digitoHexadeciamal = ["A", "B", "C", "D", "E", "F", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function clicar(){
+    let codigoHex = [];
+    codigoHex.push(numero);
+    for(let i = 0; i < 6; i++){
+        let numero = Math.random() * 17;
+        numero = Math.round( numero );
+    }
+}
