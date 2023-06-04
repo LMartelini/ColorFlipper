bb2908e93511bc80885ddd811513211a00368fd8
Laís <laismartelini03@gmail.com>
2023-06-04 15:58:32 -0300

    Aplicação de responsividade

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 let botao = document.querySelector('button').onclick = clicar;
 let div = document.querySelector('.espaço');
 
-let digitoHexadeciamal = ["A", "B", "C", "D", "E", "F", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let digitoHexadecimal = ["A", "B", "C", "D", "E", "F", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+let color = document.querySelector('#color');
 
 function clicar(){
     let codigoHex = [];
-    codigoHex.push(numero);
     for(let i = 0; i < 6; i++){
         let numero = Math.random() * 17;
         numero = Math.round( numero );
+        codigoHex.push(digitoHexadecimal[numero]);
     }
+
+    let cor = codigoHex[0] + codigoHex[1] + codigoHex[2] + codigoHex[3] + codigoHex[4] + codigoHex[5];
+    console.log(cor);
+
+    div.style.backgroundColor = '#' + cor;
+
+    color.innerHTML = '#' + cor;
 }
